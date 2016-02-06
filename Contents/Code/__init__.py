2303f20bb15183834a7db63ec782f8487330a96b
vanstinator <justinvdhooft@gmail.com>
2016-02-06 00:52:43 -0500

    first 2 levels of menus and image loading/caching

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,10 +1,11 @@
 import webtools
 
-
 PREFIX = "/applications/UASViewer"
 NAME = 'UASViewer'
 ART = 'background.jpg'
 ICON = 'plexwink.png'
+
+
 # PREFS_ICON = 'plexwink.png'
 # PROFILE_ICON = 'plexwink.png'
 
@@ -13,15 +14,41 @@ def Start():
     """ Run by Plex when the server starts
     """
     Log('Starting UASViewer')
-    ObjectContainer.title = NAME
+    ObjectContainer.title1 = NAME
+    # Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
+    # ObjectContainer.view_group = 'List'
     ValidatePrefs()
 
 
 @handler(PREFIX, NAME, art=R(ART), thumb=R(ICON))
-def MainMenu(header=NAME, message="Hello"):
+@route(PREFIX + '/MainMenu')
+def MainMenu():
     oc = ObjectContainer(no_cache=True, no_history=True, replace_parent=True)
-    for key, value in UAS.BUNDLE_TYPES:
-        oc.add(DirectoryObject(key=Callback(MainMenu), title=key))
+    for value in UAS.BUNDLE_TYPES:
+        oc.add(DirectoryObject(key=Callback(CategoryMenu, bundle_type=value), title=value))
+    return oc
+
+
+@route(PREFIX + '/Category')
+def CategoryMenu(bundle_type):
+    oc = ObjectContainer(no_cache=True, no_history=True, replace_parent=True)
+    for key, value in UAS.CHANNEL_DICT:
+        if bundle_type in value["type"]:
+            Log('http://' + Prefs['PLEX_PATH'] + ':' + Prefs['WEB_TOOLS_PORT'] + '/uas/Resources/' + value["icon"])
+            oc.add(DirectoryObject(key=Callback(CategoryMenu),
+                                   title=value["title"],
+                                   summary=value["description"],
+                                   thumb=Callback(Thumb,
+                                                  url='http://'
+                                                        + Prefs['PLEX_PATH']
+                                                        + ':'
+                                                        + Prefs['WEB_TOOLS_PORT']
+                                                        + '/uas/Resources/'
+                                                        + value["icon"]
+                                                  )
+                                   )
+                   )
+    return oc
 
 
 @route(PREFIX + '/ValidatePrefs')
@@ -31,4 +58,15 @@ def ValidatePrefs():
     global UAS
     Log('Validating Prefs')
     Log('Initializing WebTools Session')
-    UAS = webtools.WebToolsAPI(Prefs['PMS_ADDRESS'], Prefs['PMS_PORT'], Prefs['PLEX_USERNAME'], Prefs['PLEX_PASSWORD'])
+    UAS = webtools.WebToolsAPI(Prefs['PLEX_PATH'], Prefs['WEB_TOOLS_PORT'], Prefs['PLEX_USERNAME'],
+                               Prefs['PLEX_PASSWORD'])
+
+
+def Thumb(url):
+    """ Go try to get the thumbnail and load it into memory
+    """
+    try:
+        data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
+        return DataObject(data, 'image/jpeg')
+    except:
+        return Redirect(R(ICON))
--- Contents/Code/webtools.py
+++ Contents/Code/webtools.py
@@ -5,16 +5,17 @@ class WebToolsAPI:
     """ Interfaces between the UASViewer channel code and the WebTools 2.0 API
     """
 
-    SESSION = requests.session()
-    BUNDLE_TYPES = None
+    SESSION = None
+    BUNDLE_TYPES = dict()
     CHANNEL_DICT = None
 
     def __init__(self, plex_path, plex_port, plex_username, plex_password):
         global BUNDLE_TYPES, CHANNEL_DICT, SESSION
-        self.auth_session(plex_username, plex_password)
+        SESSION = requests.session()
+        self.auth_session(plex_path, plex_port, plex_username, plex_password)
         bundles = SESSION.get('http://' + plex_path + ':' + plex_port + '/webtools2?module=pms&function=getAllBundleInfo')
-        BUNDLE_TYPES = self.build_bundle_type_dict(bundles.json())
-        CHANNEL_DICT = bundles.json()
+        self.BUNDLE_TYPES = self.build_bundle_type_dict(bundles.json())
+        self.CHANNEL_DICT = bundles.json().items()
 
     @staticmethod
     def build_bundle_type_dict(json_obj):
@@ -25,6 +26,6 @@ class WebToolsAPI:
         return bundle_types
 
     @staticmethod
-    def auth_session(plex_username, plex_password):
+    def auth_session(plex_path, plex_port, plex_username, plex_password):
         payload = {'user': plex_username, 'pwd': plex_password}
-        SESSION.post('http://localhost:33400/login', data=payload)
+        SESSION.post('http://' + plex_path + ':' + plex_port + '/login', data=payload)
